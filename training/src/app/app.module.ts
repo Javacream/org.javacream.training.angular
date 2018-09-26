77c435a857e8df24c5d1e5a6d8fa815074e36f55
= <=>
2018-09-26 10:59:59 +0200

    working

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SamplePersonComponent } from './sample-person/sample-person.component';
@@ -8,7 +9,17 @@ import { PeopleListComponent } from './people-list/people-list.component';
 import { PersonInputComponent } from './person-input/person-input.component';
 import {PeopleModelService} from './people-model.service';
 import { PersonDetailComponent } from './person-detail/person-detail.component'
+
+const routes:Routes =
+  [
+    { path: 'sample', component: SamplePersonComponent },
+    { path: 'input', component: PersonInputComponent },
+    { path: 'list', component: PeopleListComponent },
+
+]
+
 @NgModule({
+  
   declarations: [
     AppComponent,
     SamplePersonComponent,
@@ -18,7 +29,9 @@ import { PersonDetailComponent } from './person-detail/person-detail.component'
   ],
   imports: [
     BrowserModule, 
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+     routes)
   ],
   providers: [PeopleModelService],
   bootstrap: [AppComponent]
