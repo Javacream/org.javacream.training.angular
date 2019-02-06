81e3cbdd2f4bf57307a19d9fc49fd3a844e9e07b
Rainer Sawitzki <training@rainer-sawitzki>
2019-02-06 16:12:06 +0100

    add navigation

--- people/src/people/people.modul.ts
+++ people/src/people/people.modul.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { PeopleMainComponent} from './people.main.component';
-import { SamplePersonComponent} from './sample-person/sample-person.component';
-import {PersonComponent} from './person/person.component';
+import { PeopleMainComponent } from './people.main.component';
+import { SamplePersonComponent } from './sample-person/sample-person.component';
+import { PersonComponent } from './person/person.component';
 import { PeopleListComponent } from './people-list/people-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
-import {MatButtonModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 import { PersonInputComponent } from './person-input/person-input.component';
 import { FormsModule } from '@angular/forms';
 import { PersonLoaderComponent } from './person-loader/person-loader.component';
 import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [{
+  path: 'list', component: PeopleListComponent
+}, { path: 'loader', component: PersonLoaderComponent }];
 @NgModule({
   declarations: [
     PeopleMainComponent, SamplePersonComponent, PersonComponent, PeopleListComponent, PersonInputComponent, PersonLoaderComponent
   ],
   imports: [
-    BrowserModule, MatButtonModule, FormsModule, HttpClientModule
+    BrowserModule, MatButtonModule, FormsModule, HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [HttpClientModule],
   bootstrap: [PeopleMainComponent]
