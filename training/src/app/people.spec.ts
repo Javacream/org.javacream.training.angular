d13df21ea9d6f5910b255e270ae5ad3e11bc0cab
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-07-28 09:39:19 +0200

    fix refactoring model directory

--- training/src/app/people.spec.ts
+++ training/src/app/people.spec.ts
@@ -1,4 +1,4 @@
-import {Person } from './model/people';
+import {Person } from './people/model/people';
 it('literal is a person', () => {
   let p:Person = {id: 1, lastname:"A", firstname:"A", gender:"A", height:188}
   expect(p.lastname).toBe("A");
