8eb5d8025e7faf1b6fa8eebcf6da7a81c4fab2df
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-07-28 10:13:12 +0200

    introduce application context

---
+++ training/src/app/ApplicationContext.ts
@@ -0,0 +1,4 @@
+import {Person, PersonClass} from './people/model/people'
+
+export const company = "Javacream from Application Context"
+export let user = new PersonClass(42, "Mustermann", "Hans", 'm', 199)
--- training/src/app/common/component/footer/footer.component.ts
+++ training/src/app/common/component/footer/footer.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {company} from '../../../ApplicationContext'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  footerText = company
+  constructor() {
 
-  constructor() { }
-
+   }
   ngOnInit(): void {
   }
 
