16987e2f8194b855955db3c59fb2f8e91528d496
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-07-29 13:40:51 +0200

    implement http client in Component class

--- training/src/app/people/component/person-search/person-search.component.ts
+++ training/src/app/people/component/person-search/person-search.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from '../../model/people';
 import {PeopleServerControllerService} from '../../services/people-server-controller.service'
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-person-search',
   templateUrl: './person-search.component.html',
   styleUrls: ['./person-search.component.css']
 })
 export class PersonSearchComponent implements OnInit {
+  ENDPOINT = "http://10.28.6.1:8080/people"
 
   id: number;
   person: Person;
-  constructor(private controller: PeopleServerControllerService) { }
+  constructor(private controller: PeopleServerControllerService,  readonly http:HttpClient) { }
 
   ngOnInit() {
   }
@@ -20,6 +23,10 @@ export class PersonSearchComponent implements OnInit {
   }
   loadSubscription() {
     this.controller.findByIdHttpClient(this.id, (person) => this.person = person);
+  }
+
+  loadSubscriptionDirect(){
+    this.http.get<Person>(`${this.ENDPOINT}/${this.id}`).subscribe((person) => this.person = person)
   }
 
 }
