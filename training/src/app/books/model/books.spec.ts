1c83b76a04b9655ed7452f68c6d45dbf04258c5c
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-08-05 14:55:37 +0200

    introduce BooksController

--- training/src/app/books/model/books.spec.ts
+++ training/src/app/books/model/books.spec.ts
@@ -1,4 +1,4 @@
-import {Book, BookClass} from './books'
+import {Book, BookClass, BooksController} from './books'
 
 let jsonData:Book = {
     isbn: "ISBN",
@@ -12,13 +12,13 @@ describe('Books Model', () => {
     it('book class is working', () => {
         let aBook = new BookClass("ISBN", "Title", 200, 19.99, true)
         expect(aBook.isbn).toBe("ISBN");
-      }),
-      it('info is working', () => {
+    }),
+    it('info is working', () => {
         let aBook = new BookClass("ISBN", "Title", 200, 19.99, true)
         expect(aBook.info()).toBe("Book: isbn=ISBN, title=Title");
-      }),
+    }),
 
-      it('json is working', () => {
+    it('json is working', () => {
         let aBook:Book = {
             isbn: "ISBN",
             title:"JSON",
@@ -27,6 +27,38 @@ describe('Books Model', () => {
             available: true
         }
         expect(aBook.isbn).toBe("ISBN");
-      })
- 
-  })
+    })
+})
+describe('Books Controller', () => {
+    const title = "Typescript"
+    let generatedIsbn = ""
+    let booksController:BooksController = null
+    beforeEach(() => {
+        booksController = new BooksController()
+        generatedIsbn = booksController.create(title, 400, 29.99, true)
+    });
+  
+    it('controller generates a book', () => {
+        expect(generatedIsbn).not.toBeNull();
+    }),
+    it('controller finds a book', () => {
+        let book = booksController.findBookByIsbn(generatedIsbn)
+        expect(book.title).toBe(title);
+    }),
+    it('controller retrieves list of books', () => {
+        let book = booksController.findBookByIsbn(generatedIsbn)
+        let books = booksController.findAll()
+        expect(books).toContain(book)
+    }),
+    it('controller generates list of titles', () => {
+        let booksTitles = booksController.findAllTitles()
+        expect(booksTitles).toContain(title)
+    }),
+    it('controller generates list of titles', () => {
+        let book = booksController.findBookByIsbn(generatedIsbn)
+        booksController.deleteByIsbn(generatedIsbn)
+        let books = booksController.findAll()
+        expect(books).not.toContain(book)
+    })
+   
+})
--- training/src/app/books/model/books.ts
+++ training/src/app/books/model/books.ts
@@ -33,3 +33,31 @@ export class BookClass implements Book{
         return `Book: isbn=${this.isbn}, title=${this.title}`
     }
 }
+
+export class BooksController{
+    books = new Map<string, Book>()
+    counter = 0
+    create(title:string, pages:number, price:number, available:boolean):string{
+        this.counter++
+        let book = new BookClass(`ISBN-${this.counter}`, title, pages, price, available)
+        this.books.set(book.isbn, book)
+        return book.isbn
+    }
+
+    findBookByIsbn(isbn:string):Book{
+        return this.books.get(isbn)
+    }
+	findAll(): Array<Book> {
+        return Array.from(this.books.values())
+    }
+ 	update(book: Book):void{
+        this.books.set(book.isbn, book)
+     }
+	deleteByIsbn(isbn:string){
+        this.books.delete(isbn)
+    }
+	findAllTitles(): Array<string>{
+        return Array.from(this.books.values()).map((book) => book.title)
+    }
+
+}
