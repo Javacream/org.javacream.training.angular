fe9976f5b3b69dc5ae192e0230829ae18c03d2d3
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-08-05 15:46:17 +0200

    first components

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './books/components/header/header.component';
+import { BookContentComponent } from './books/components/book-content/book-content.component';
+import { FooterComponent } from './books/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
