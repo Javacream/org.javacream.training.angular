b90b928f350ce534042f07b795d154352e514cf2
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-09-16 16:09:52 +0200

    start testing

--- training/src/app/books/services/books.service.spec.ts
+++ training/src/app/books/services/books.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BooksService } from './books.service';
 
 describe('BooksService', () => {
   let service: BooksService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient],
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(BooksService);
   });
 
---
+++ training/src/app/simple.spec.ts
@@ -0,0 +1,10 @@
+describe('SIMPLE', () => {
+    it('should work', () => {
+      expect(true).toBeTruthy();
+    });
+    it('should work too', () => {
+        expect(true).toBeTruthy();
+      });
+    
+  });
+  
