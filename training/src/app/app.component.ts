35945485795a86857f383c9ca29017eadea03e5d
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-11 16:13:16 +0100

    implement simple profile

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {profile} from './model/people.data'
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'training';
+
+  profileGreeting(){
+    return `Hello ${profile.firstname} ${profile.lastname}`
+  }
 }
 
---
+++ training/src/app/model/people.data.ts
@@ -0,0 +1,3 @@
+import {Person} from './people.model'
+
+export let profile:Person = {id:42, lastname:"Sawitzki", firstname:"Rainer", gender:"m", height:183}
