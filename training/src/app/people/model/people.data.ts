821dc117835be78da0e73e83bbb348d20a641c92
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-12 13:31:13 +0100

    introduce collections

--- training/src/app/people/model/people.data.ts
+++ training/src/app/people/model/people.data.ts
@@ -6,4 +6,16 @@ appName: "P E O P L E",
 company: "Javacream"
 }
 
+export let people = new Map<number, Person>()
+
+people.set (1, create(1, "Sawitzki", "Rainer", "m", 183))
+people.set (2, create(2, "Meier", "Hanna", "f", 175))
+people.set (3, create(3, "Meier", "Franz", "m", 186))
+people.set (4, create(4, "Metzger", "Johann", "m", 122))
+people.set (5, create(5, "Schneider", "Erna", "f", 199))
+
+function create(id:number, lastname:string, firstname:string, gender:string, height:number){
+    return {id, lastname, firstname, gender, height}
+}
+
 //setTimeout(() => {profile.firstname = "Klaus"; console.log("Changed firstname: " + profile.firstname)}, 1000)
--- training/src/app/people/model/people.model.spec.ts
+++ training/src/app/people/model/people.model.spec.ts
@@ -1,7 +1,26 @@
 import {Person} from './people.model'
+import {people} from './people.data'
 describe("PeopleModel", () => {
     it ("Person interface works", () => {
         const person:Person = {id:42, lastname:"Sawitzki", firstname:"Rainer", gender:"m", height:183}
         expect(person.lastname).toBe("Sawitzki")
     })
+})
+
+describe("People Map", () => {
+    it ("people has 5 elements", () => {
+        expect(people.size).toBe(5)
+    })
+    it ("people has 2 Meier", () => {
+        const peopleList = Array.from(people.values())
+        const filtered = peopleList.filter((p) => p.lastname === 'Meier')
+        expect(filtered.length).toBe(2)
+    })
+
+    it ("peopleList can be transformed to Names", () => {
+        const peopleList = Array.from(people.values())
+        const filtered = peopleList.filter((p) => p.lastname === 'Schneider').map(person => `${person.firstname} ${person.lastname}`)
+        expect(filtered[0]).toBe("Erna Schneider")
+    })
+
 })
