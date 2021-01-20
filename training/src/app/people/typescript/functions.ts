cb4900fe3814c25673a3c42b5f7ca38bad16951d
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-20 15:17:42 +0100

    typescript Part2 : functions

---
+++ training/src/app/people/typescript/functions.ts
@@ -0,0 +1,48 @@
+//klassisch
+
+function demo(p1:string, p2:boolean){
+    return 42
+}
+
+
+let result = demo("Hugo", true)
+//demo("Hugo", 42)
+
+function demo2(p1:string, p2:boolean):number{
+    return 42
+}
+
+
+function demo3(p1:string, p2:boolean){
+    if(p2){
+        return 42
+    }else{
+        return "OK"
+    }
+}
+
+let result3 = demo3("", true)
+
+
+
+//Funktions-Literal
+
+let demoLiteral = function (p1:string, p2:boolean){
+    return 42
+}
+
+let literalResult = demoLiteral("Hugo", true)
+
+//Funktions-Literal und Typisierung
+
+let demoLiteral2: (x:string, y:boolean) => number
+
+demoLiteral2 = function (p1:string, p2:boolean){
+    return 42
+}
+
+//Arrow-Syntax
+
+let demoArray = (p1:string, p2:boolean) => {
+    return 42
+}
