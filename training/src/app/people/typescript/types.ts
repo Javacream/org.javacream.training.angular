e0ade0a67de10c273221625588127acb15838821
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-20 14:56:53 +0100

    typescript Part 1: types

---
+++ training/src/app/people/typescript/types.ts
@@ -0,0 +1,18 @@
+let message:string = 'Hello Hugo'
+message = "Hugo"
+//message = 42 Compiler Fehler: Typen passen  nicht
+
+//Type Inference
+let message2:string 
+
+message2 = "Emil"
+//message2 = 42 Compiler Fehler: Typen passen  nicht
+
+
+//any type
+let message3 
+
+message3 = "Emil"
+message3 = 42
+
+
