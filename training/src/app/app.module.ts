695ccd125570bdc36602b2f8786ce2c282585163
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-20 13:27:54 +0100

    finish implementinf simple ui

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './people/components/header/header.component';
+import { FooterComponent } from './people/components/footer/footer.component';
+import { ContentComponent } from './people/components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
