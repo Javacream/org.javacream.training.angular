6cbb6a73b263b56c80a556646c948228093fb5a5
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-21 11:58:59 +0100

    introduce PeopleList Component

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './people/components/header/header.component';
 import { FooterComponent } from './people/components/footer/footer.component';
 import { ContentComponent } from './people/components/content/content.component';
+import { PeoplelistComponent } from './people/components/peoplelist/peoplelist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    PeoplelistComponent
   ],
   imports: [
     BrowserModule
---
+++ training/src/app/people/components/peoplelist/peoplelist.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { PeopleController, Person } from '../../model/people.model';
+
+@Component({
+  selector: 'app-peoplelist',
+  templateUrl: './peoplelist.component.html',
+  styleUrls: ['./peoplelist.component.css']
+})
+export class PeoplelistComponent implements OnInit {
+
+  peopleList:Array<Person>
+  constructor() { 
+
+    let peopleController:PeopleController
+    this.peopleList = peopleController.findAll()
+  }
+
+  ngOnInit(): void {
+  }
+
+
+}
