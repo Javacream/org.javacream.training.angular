06d4395faf61497e4f53ac54cab7f3526b0ed00b
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-21 09:48:27 +0100

    introduce people model and tests

---
+++ training/src/app/people/model/people.model.spec.ts
@@ -0,0 +1,7 @@
+describe ("Person type works", () => {
+	it ("person has id, lastname, ...", () => {
+		let p = {id:100, lastname: "Sawitzki"}
+		expect(p.lastname).toBe("Sawitzki")
+})
+
+})
---
+++ training/src/app/people/model/people.model.ts
@@ -0,0 +1,7 @@
+interface Person{
+    id:number
+    lastname:string
+    firstname:string
+    gender:string
+    height:number
+}
