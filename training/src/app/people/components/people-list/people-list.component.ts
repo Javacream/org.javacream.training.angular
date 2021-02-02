277b39f199fecc68000219a90835488c47c9c258
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-02 14:22:29 +0100

    finish PeopleList

--- training/src/app/people/components/people-list/people-list.component.ts
+++ training/src/app/people/components/people-list/people-list.component.ts
@@ -8,11 +8,14 @@ import { PeopleService, Person } from '../../model/people.model';
 })
 export class PeopleListComponent implements OnInit {
   peopleList:Array<Person>
+  showPersonDetail = true
   constructor(readonly peopleService:PeopleService) {
     this.peopleList = peopleService.findAll()
    }
 
   ngOnInit(): void {
   }
-
+  switchDetails(){
+    this.showPersonDetail = !this.showPersonDetail
+  }
 }
