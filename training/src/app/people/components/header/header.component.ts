408a87320b44804e1f9c7ea19fbbaa299e163ffc
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-02 09:33:47 +0100

    add component attributes and interpolation directive

--- training/src/app/people/components/header/header.component.ts
+++ training/src/app/people/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Person} from '../../model/people.model'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  appTitle = "P E O P L E"
+  profile:Person = {id:1000, lastname:"Sawitzki", firstname: "Rainer", gender: "m", height:183}
   constructor() { }
 
   ngOnInit(): void {
