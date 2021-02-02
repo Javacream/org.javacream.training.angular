92a311493f404c7608d0c7910030d14c65ae6385
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-02 15:23:26 +0100

    implement person search

---
+++ training/src/app/people/components/person-search/person-search.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PeopleService, Person } from '../../model/people.model';
+
+@Component({
+  selector: 'app-person-search',
+  templateUrl: './person-search.component.html',
+  styleUrls: ['./person-search.component.css']
+})
+export class PersonSearchComponent implements OnInit {
+  searchId=0
+  personResult:Person|undefined
+  constructor(readonly peopleService:PeopleService) { }
+
+  ngOnInit(): void {
+  }
+
+  search(){
+    this.personResult = this.peopleService.findById(this.searchId)
+  }
+
+}
