30c5122d56d83556b6f92177092e926920d391ce
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-17 15:45:32 +0100

    implement simple people ui

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './people/components/header/header.component';
+import { ContentComponent } from './people/components/content/content.component';
+import { FooterComponent } from './people/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
