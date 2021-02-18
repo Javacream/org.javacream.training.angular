4c912ab8031cf451e78d5109a26e62afda978006
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-18 14:00:22 +0100

    implement Person details

--- training/src/app/people/components/person/person.component.ts
+++ training/src/app/people/components/person/person.component.ts
@@ -8,6 +8,7 @@ import { Person } from '../../model/people.model';
 })
 export class PersonComponent implements OnInit {
   @Input() person:Person
+  @Input() details = false
   constructor() { }
 
   ngOnInit(): void {
