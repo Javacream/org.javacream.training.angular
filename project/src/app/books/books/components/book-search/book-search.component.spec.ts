a71f9429663e09624ebad9d9074b906422651e4f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-21 14:11:59 +0200

    play with Mocks in component test

--- project/src/app/books/books/components/book-search/book-search.component.spec.ts
+++ project/src/app/books/books/components/book-search/book-search.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BooksService } from '../../services/books.service';
 
 import { BookSearchComponent } from './book-search.component';
 
@@ -9,8 +10,9 @@ describe('BookSearchComponent', () => {
   let fixture: ComponentFixture<BookSearchComponent>;
 
   beforeEach(async(() => {
+    let booksServiceMock = jasmine.createSpyObj(BooksService, ['findByIsbn'])
     TestBed.configureTestingModule({
-      declarations: [ BookSearchComponent ], imports: [ReactiveFormsModule, HttpClientModule]
+      declarations: [ BookSearchComponent ], imports: [ReactiveFormsModule], providers: [{provide: BooksService, useValue: booksServiceMock}]
     })
     .compileComponents();
   }));
