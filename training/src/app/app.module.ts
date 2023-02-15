c59b3ee4387acb42edd7c5e821c32b9a792231ef
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-02-15 14:16:20 +0100

    add static people ui

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './people/header/header.component';
+import { FooterComponent } from './people/footer/footer.component';
+import { ContentComponent } from './people/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
