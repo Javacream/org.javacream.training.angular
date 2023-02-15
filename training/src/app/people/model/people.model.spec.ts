201f32dccf3920e81dcb00191703079422be510f
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-02-15 16:09:21 +0100

    play with types

--- training/src/app/people/model/people.model.spec.ts
+++ training/src/app/people/model/people.model.spec.ts
@@ -1,4 +1,4 @@
-import { Person } from "./people.model"
+import { Person, PersonIF } from "./people.model"
 
 describe("people model", () => {
     xit("is OK", () => {
@@ -10,6 +10,18 @@ describe("people model", () => {
         let p = new Person(42, "Sawitzki", "Rainer", 'm', 183)
         expect(p).toBeDefined()
         expect(p.lastname).toBe("Sawitzki")
+    })  
 
-    })
+    it("is a person", () => {
+        let o = {hugo: "Fritz"}
+        o.hugo = "Emil"
+
+        let p2:PersonIF = {id: 9, lastname: 'Muster', firstname: '', gender: 'x', height: 183}
+        expect(o).toBeDefined()
+        let p = new Person(42, "Sawitzki", "Rainer", 'm', 183)
+        expect(p).toBeDefined()
+        expect(p.lastname).toBe("Sawitzki")
+    })  
+
+
 })
--- training/src/app/people/model/people.model.ts
+++ training/src/app/people/model/people.model.ts
@@ -1,16 +1,14 @@
 //Datenmodell für die geplante People Applikation
 
-export class Person{
-    constructor(id:number, lastname:string, firstname:string, gender:string, height:number){
-        this.id  = id;
-        this.firstname = firstname
-        this.lastname = lastname
-        this.height = height
-        this.gender = gender
-    }
+// syntactic sugar
+export class Person implements PersonIF{
+    constructor(readonly id:number, public lastname:string, readonly firstname:string, readonly gender:string, public height:number){}
+}
+
+export interface PersonIF {
     id:number
     lastname:string
     firstname:string
     height:number
-    gender:string
+    gender: string
 }
