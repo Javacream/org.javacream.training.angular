91aa56088e6348fd0be2374a250869e0a3ef5874
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-02-17 13:48:10 +0100

    introduce server

--- training/src/app/people/people-server-list/people-server-list.component.ts
+++ training/src/app/people/people-server-list/people-server-list.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { PersonIF } from '../model/people.model';
+import { PeopleService } from '../services/people.service';
 @Component({
   selector: 'app-people-server-list',
   templateUrl: './people-server-list.component.html',
   styleUrls: ['./people-server-list.component.css']
 })
 export class PeopleServerListComponent {
-  constructor(readonly httpClient:HttpClient){}
+  people:Array<PersonIF> = []
+  constructor(readonly peopleService:PeopleService){
+     peopleService.findAllPeopleFromServer( (serverPeople) => {this.people = serverPeople})
+
+  }
 
 }
--- training/src/app/people/services/people.service.ts
+++ training/src/app/people/services/people.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { WhiteboardService } from 'src/app/util/services/whiteboard.service';
 import { PersonIF } from '../model/people.model';
@@ -7,7 +8,7 @@ import { PersonIF } from '../model/people.model';
 })
 export class PeopleService {
   id = 0
-  constructor(readonly whiteboard: WhiteboardService) { }
+  constructor(readonly whiteboard: WhiteboardService, readonly httpClient:HttpClient) { }
 
   people:Map<number, PersonIF> = new Map([
       [1000, {id:1000, lastname:"Sawitzki", firstname:"Rainer", gender:"m", height:183}],
@@ -31,5 +32,11 @@ export class PeopleService {
     this.people.delete(id)
     this.whiteboard.personDeletion.next(id)
 
+  }
+
+  findAllPeopleFromServer(callbackFunction:(_:Array<PersonIF>) => void):void{
+    const url = "http://javacream.eu:8080/people"
+    const observable = this.httpClient.get<Array<PersonIF>>(url)
+    observable.subscribe((people) => callbackFunction(people))
   }
 }
