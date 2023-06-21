a19d6c0cab3beee4d690854784abde51db90d28b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-06-21 15:51:57 +0200

    add raw components

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './people/components/header/header.component';
+import { FooterComponent } from './people/components/footer/footer.component';
+import { ContentComponent } from './people/components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
