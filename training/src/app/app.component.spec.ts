3fd06156a827f4568ac5a08ac318f95b9bf2c9a0
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-06-22 10:29:11 +0200

    introduce tests

--- training/src/app/app.component.spec.ts
+++ training/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './people/components/header/header.component';
+import { ContentComponent } from './people/components/content/content.component';
+import { FooterComponent } from './people/components/footer/footer.component';
+import { PeopleListComponent } from './people/components/people-list/people-list.component';
 
-xdescribe('AppComponent', () => {
+describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [AppComponent,HeaderComponent, ContentComponent, FooterComponent, PeopleListComponent]
   }));
 
   it('should create the app', () => {
@@ -21,7 +25,7 @@ xdescribe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('training app is running!');
+    const htmlElement = fixture.nativeElement as HTMLElement;
+    expect(htmlElement.querySelector('span')?.textContent).toContain('Hu');
   });
 });
--- training/src/app/people/components/content/content.component.spec.ts
+++ training/src/app/people/components/content/content.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContentComponent } from './content.component';
+import { PeopleListComponent } from '../people-list/people-list.component';
 
 describe('ContentComponent', () => {
   let component: ContentComponent;
@@ -8,7 +9,7 @@ describe('ContentComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ContentComponent]
+      declarations: [ContentComponent, PeopleListComponent]
     });
     fixture = TestBed.createComponent(ContentComponent);
     component = fixture.componentInstance;
