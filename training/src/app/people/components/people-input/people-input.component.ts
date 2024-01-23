77dc0a90f1c8abeb65998503c440cde01ff358c5
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-23 16:36:07 +0100

    add form module

--- training/src/app/people/components/people-input/people-input.component.ts
+++ training/src/app/people/components/people-input/people-input.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-people-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './people-input.component.html',
   styleUrl: './people-input.component.css'
 })
 export class PeopleInputComponent {
+  inputLastname:string = ''
+  inputFirstname:string = ''
+
   save(){
-    console.log("save action")
+    console.log("save action, lastname=" + this.inputLastname + ", firstname=" + this.inputFirstname)
   }
 
 }
