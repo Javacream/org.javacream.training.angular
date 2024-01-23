4dcb5ee328acdb9b51e58bb8868510f1011d267c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-23 14:44:40 +0100

    finish PeopleList

--- training/src/app/people/components/people-list/people-list.component.ts
+++ training/src/app/people/components/people-list/people-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { PeopleService } from '../../services/people.service';
+import { Person } from '../../model/people';
+import { PersonComponent } from '../person/person.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-people-list',
   standalone: true,
-  imports: [],
+  imports: [PersonComponent, CommonModule],
   templateUrl: './people-list.component.html',
   styleUrl: './people-list.component.css'
 })
 export class PeopleListComponent {
-
+  peopleList:Array<Person>
+  constructor(readonly peopleService:PeopleService){
+    this.peopleList = peopleService.findAll()
+  }
 }
