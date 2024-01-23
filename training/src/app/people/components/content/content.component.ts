135098ea2be4850c98c3ba16b4606f55c49d354f
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-23 14:19:18 +0100

    start people list component

--- training/src/app/people/components/content/content.component.ts
+++ training/src/app/people/components/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PeopleListComponent } from '../people-list/people-list.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [PeopleListComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
