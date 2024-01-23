a8e7e3fe6ad4c26e5eba72dba552b59bfbf5646b
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-23 15:55:10 +0100

    finish clock

--- training/src/app/util/components/clock/clock.component.ts
+++ training/src/app/util/components/clock/clock.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clock.component.html',
   styleUrl: './clock.component.css'
 })
-export class ClockComponent {
-
+export class ClockComponent implements OnInit, OnDestroy{
+  actualTime = new Date()
+  interval: any
+  ngOnInit():void{
+    this.interval = setInterval(() => {
+      this.actualTime = new Date()
+    }, 1000)
+  }
+  ngOnDestroy():void{
+    clearInterval(this.interval)
+  }
 }
