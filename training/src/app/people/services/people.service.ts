1611381f68b0710b7198ff778644223597b29b6d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-23 13:53:03 +0100

    introduce Map and Array

--- training/src/app/people/services/people.service.ts
+++ training/src/app/people/services/people.service.ts
@@ -6,8 +6,17 @@ import { Person } from '../model/people';
 })
 export class PeopleService {
   user:Person  = {id: 1000, lastname:'Mustermann', firstname: 'Max'}
-  constructor() { }
+  peopleMap: Map<number, Person>
+  constructor() { 
+    this.peopleMap = new Map()
+    this.peopleMap.set(100, {id: 100, lastname: 'Sawitzki', firstname: 'Rainer'})
+    this.peopleMap.set(101, {id: 101, lastname: 'Metzger', firstname: 'Hannah'})
+    this.peopleMap.set(102, {id: 102, lastname: 'Sawitzki', firstname: 'Andrea'})
+  }
   getUser():Person {
     return this.user
+  }
+  findAll():Array<Person>{
+    return Array.from(this.peopleMap.values())
   }
 }
