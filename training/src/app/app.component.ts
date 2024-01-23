d636a2e33b0037cc6600a791337f2188a4d40adf
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-23 09:31:41 +0100

    add people components

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { demoFunction } from './demo';
+import { ContentComponent } from './people/components/content/content.component';
+import { FooterComponent } from './people/components/footer/footer.component';
+import { HeaderComponent } from './people/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
