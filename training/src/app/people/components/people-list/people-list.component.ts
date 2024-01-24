410b26e7240c6d7b02fa4d8dab73843395539616
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-24 10:30:48 +0100

    subscriptions and component lifecycle

--- training/src/app/people/components/people-list/people-list.component.ts
+++ training/src/app/people/components/people-list/people-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PeopleService } from '../../services/people.service';
 import { Person } from '../../model/people';
 import { PersonComponent } from '../person/person.component';
 import { CommonModule } from '@angular/common';
 import { WhiteboardService } from '../../../util/services/whiteboard.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-people-list',
@@ -12,11 +13,20 @@ import { WhiteboardService } from '../../../util/services/whiteboard.service';
   templateUrl: './people-list.component.html',
   styleUrl: './people-list.component.css'
 })
-export class PeopleListComponent {
+export class PeopleListComponent implements OnInit, OnDestroy {
   peopleList:Array<Person>
+  personCreatedSubscription?:Subscription
   constructor(readonly peopleService:PeopleService, readonly whiteBoardService: WhiteboardService){
     this.peopleList = peopleService.findAll()
-    whiteBoardService.personCreated_channel.subscribe(p => this.peopleList = this.peopleService.findAll())
+  }
+
+  ngOnInit(): void {
+    this.personCreatedSubscription = this.whiteBoardService.personCreated_channel.subscribe(p => this.peopleList = this.peopleService.findAll())
+    
+  }
+
+  ngOnDestroy(): void {
+    this.personCreatedSubscription!.unsubscribe()
   }
 
 }
