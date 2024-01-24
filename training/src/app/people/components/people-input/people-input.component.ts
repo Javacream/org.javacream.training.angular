91a080a235da7fcba508bb691771be1f5f77a0b8
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-01-24 11:48:01 +0100

    introduce reactive forms

--- training/src/app/people/components/people-input/people-input.component.ts
+++ training/src/app/people/components/people-input/people-input.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PeopleService } from '../../services/people.service';
 
 @Component({
   selector: 'app-people-input',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './people-input.component.html',
   styleUrl: './people-input.component.css'
 })
 export class PeopleInputComponent {
-  inputLastname:string = ''
-  inputFirstname:string = ''
+  personInputForm = new FormGroup({
+    lastname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
+    firstname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(1)]))
 
+  })
   constructor(readonly peopleService:PeopleService){}
   save(){
-    let created = this.peopleService.create(this.inputLastname, this.inputFirstname)
+    let created = this.peopleService.create(this.personInputForm.value.lastname!, this.personInputForm.value.firstname!)
   }
 
 }
