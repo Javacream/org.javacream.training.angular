5a6ced066f34f74435d417062fdd1df3a1673820
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-10-21 15:12:09 +0200

    extend PersonComponent with detail

--- training/src/app/people/person-component/person-component.ts
+++ training/src/app/people/person-component/person-component.ts
@@ -1,13 +1,14 @@
 import { Component, Input} from '@angular/core';
 import { Person } from '../person';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-person',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './person-component.html',
   styleUrl: './person-component.css'
 })
 export class PersonComponent {
   @Input() person?:Person
-
+  @Input() detail?:boolean
 }
